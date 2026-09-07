0ee24f6214c186d7bdf7ca79d15765514eb89b14
chaitali-tekdi <chaitali.shinde@tekditechnologies.com>
2026-09-07 19:18:06 +0530

    Issue #263965 Fix: Update Role-Based Validation for Organization, Position, Province & Site Fields , Log-visit

--- src/screens/ParticipantDetail/LogVisitModulePopup.tsx
+++ src/screens/ParticipantDetail/LogVisitModulePopup.tsx
@@ -119,13 +119,13 @@ function LogVisitModulePopupComponent({
     [],
   );
 
-const isDropoutRequestSentForApproval = useCallback(() => {
-  return Boolean(
-    participant?.pendingChangeRequest?.some(
-      (request: any) =>  request.action === 'PROGRAM_USER_DROPPING_OUT' && request.status === 'PENDING'
-    )
-  );
-}, [participant?.pendingChangeRequest]);
+  const isDropoutRequestSentForApproval = useMemo(() => {
+    return Boolean(
+      participant?.pendingChangeRequest?.some(
+        (request: any) => request.action === 'PROGRAM_USER_DROPPING_OUT' && request.status === 'PENDING'
+      )
+    );
+  }, [participant?.pendingChangeRequest]);
 
   const handleOpenLogVisit = useCallback((isOpenf:"expand" | "openForm" | "openList" = "expand") => {
     if(isOpenf === "expand") {
